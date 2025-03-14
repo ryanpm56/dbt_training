90f06c73322535597be86aa8fc7f6c3a19b2b097
Ryan Momberger <momberger_ryan@bah.com>
2025-03-14 16:40:14 +0000

    added seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 --from raw orders
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
 o.orderid,
 o.orderdate, 
 o.shipdate,
@@ -16,11 +17,13 @@ p.productid,
 p.category, 
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice','ordercostprice')}} as markup
+{{ markup('ordersellingprice','ordercostprice')}} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customers') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_products') }} as p
 on o.productid = p.productid
-
+left join {{'delivery_team'}} as d
+on o.shipmode = d.shipmode
 	
